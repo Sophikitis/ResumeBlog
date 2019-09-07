4f11a2f4d195a6c06106d37f76e90b3dba5f803d
Jeremie Soffichiti <jeremie.soffichiti@gmail.com>
2019-09-08 00:36:17 +0200

    installing webpack encore with jquery and bootstrap

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+require('../css/app.scss');
+import $ from 'jquery';
+import 'popper.js'
+import 'bootstrap'
+require('bootstrap/dist/css/bootstrap.css');
+
+
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
