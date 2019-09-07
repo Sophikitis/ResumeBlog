47ae61e0c847ebf70569879a2556482606b4b3eb
Jeremie Soffichiti <jeremie.soffichiti@gmail.com>
2019-09-08 00:45:38 +0200

    create simple entity resumeMe (only firstname)

---
+++ src/Repository/ResumeMeRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ResumeMe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method ResumeMe|null find($id, $lockMode = null, $lockVersion = null)
+ * @method ResumeMe|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ResumeMe[]    findAll()
+ * @method ResumeMe[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ResumeMeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ResumeMe::class);
+    }
+
+    // /**
+    //  * @return ResumeMe[] Returns an array of ResumeMe objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('r.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?ResumeMe
+    {
+        return $this->createQueryBuilder('r')
+            ->andWhere('r.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
